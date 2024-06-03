f588b6c41835eed7b31bf9356bc1f9c30b38f1c3
Brayan Moreno <112002842+UTS23@users.noreply.github.com>
2024-06-03 19:38:17 +0000

    CAMBIOS2

--- src/main/java/com/papeleria/app/PapeleriaApplication.java
+++ src/main/java/com/papeleria/app/PapeleriaApplication.java
@@ -8,13 +8,16 @@ public class PapeleriaApplication {
 
     public static void main(String[] args) {
         // Configurar TrustStore y KeyStore
-        System.setProperty("javax.net.ssl.trustStore", "ruta/a/tu/truststore.jks");
-        System.setProperty("javax.net.ssl.trustStorePassword", "contrasenaTruststore");
-        System.setProperty("javax.net.ssl.keyStore", "ruta/a/tu/keystore.jks");
-        System.setProperty("javax.net.ssl.keyStorePassword", "contrasenaKeystore");
+        System.setProperty("javax.net.ssl.trustStore", "src/main/resources/truststore.jks");
+        System.setProperty("javax.net.ssl.trustStorePassword", "truststore-password");
+        System.setProperty("javax.net.ssl.keyStore", "src/main/resources/keystore.jks");
+        System.setProperty("javax.net.ssl.keyStorePassword", "keystore-password");
 
         // Habilitar depuración SSL/TLS
         System.setProperty("javax.net.debug", "ssl,handshake");
+
+        // Configurar protocolos SSL/TLS
+        System.setProperty("https.protocols", "TLSv1.2,TLSv1.3");
 
         // Ejecutar la aplicación Spring Boot
         SpringApplication.run(PapeleriaApplication.class, args);
