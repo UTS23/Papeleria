8ee3c0db3ec0daea277b43ed8fdacfc0f41601c3
Brayan Moreno <112002842+UTS23@users.noreply.github.com>
2024-06-03 19:27:12 +0000

    CAMBIOS

--- src/main/java/com/papeleria/app/PapeleriaApplication.java
+++ src/main/java/com/papeleria/app/PapeleriaApplication.java
@@ -6,8 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PapeleriaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PapeleriaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Configurar TrustStore y KeyStore
+        System.setProperty("javax.net.ssl.trustStore", "ruta/a/tu/truststore.jks");
+        System.setProperty("javax.net.ssl.trustStorePassword", "contrasenaTruststore");
+        System.setProperty("javax.net.ssl.keyStore", "ruta/a/tu/keystore.jks");
+        System.setProperty("javax.net.ssl.keyStorePassword", "contrasenaKeystore");
 
+        // Habilitar depuración SSL/TLS
+        System.setProperty("javax.net.debug", "ssl,handshake");
+
+        // Ejecutar la aplicación Spring Boot
+        SpringApplication.run(PapeleriaApplication.class, args);
+    }
 }
